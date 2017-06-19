7701214140da64ca456ddd3207c44a5fe304a207
NikhilMara <nikhilreddymara@gmail.com>
2017-06-19 17:56:20 +0200

    arrays and accessing array elements

---
+++ Array.java
@@ -0,0 +1,22 @@
+package com.BreakContinue;
+
+
+public class Array {
+
+public static void main(String args[]){
+
+    int num[] = {1,2,3};
+    for (int i=0;i<num.length;i++){
+      System.out.println("The number is :"+num[i]);
+
+    }
+}
+}
+
+/*output:
+
+The number is :1
+The number is :2
+The number is :3
+
+*/
