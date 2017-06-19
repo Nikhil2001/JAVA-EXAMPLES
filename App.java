fa5f7f502ad689058f126acfae4421c8ccb22bf7
NikhilMara <nikhilreddymara@gmail.com>
2017-06-19 12:30:50 +0200

    sample java class

---
+++ App.java
@@ -0,0 +1,24 @@
+package com.BasicSyntax;
+
+//This is a single line comment
+
+
+/*
+ 
+ multi line comments
+
+*/
+
+
+
+//class definition, this has to be in App.java
+
+public class App {
+
+// this method is starting point of execution ,main()
+public static void main(String args[]){
+    System.out.println("Hello World !");
+//prints hello world when executed
+}
+
+}
