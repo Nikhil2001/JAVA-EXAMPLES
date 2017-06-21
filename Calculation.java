0d2da3a48f37fa60e933190aada5bb8c2b7541ca
NikhilMara <nikhilreddymara@gmail.com>
2017-06-21 20:28:21 +0200

    simple method for arithemetic operations

---
+++ Calculation.java
@@ -0,0 +1,28 @@
+package com.method;
+
+/**
+ * Created by enilmaa on 2017-06-21.
+ */
+public class Calculation {
+    public static int calculation(int x, int y, String operation){
+        if(operation=="add"){
+            return x+y;
+        }
+        else if(operation=="sub"){
+            return x-y;
+        }
+        else if(operation=="mul"){
+            return x*y;
+        }else {
+            return 0;
+        }
+
+    }
+//this method defaults assumes no same numbers are passed to avoid return 0 when sub operation was performed
+    public static void main(String args[]){
+         System.out.println(calculation(1,2,"add"));
+        System.out.println(calculation(1,2,"mul"));
+        System.out.println(calculation(1,2,"sub"));
+    }
+}
+
