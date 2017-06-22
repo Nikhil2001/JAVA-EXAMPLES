020547f0fe382c8cf3d8853d8b012c6e1f99beb3
NikhilMara <nikhilreddymara@gmail.com>
2017-06-22 19:25:55 +0200

    Inheritence ,method override

---
+++ Inheritence/Main.java
@@ -0,0 +1,13 @@
+package Inheritence;
+
+/**
+ * Created by enilmaa on 2017-06-22.
+ */
+public class Main {
+public static void main(String[] args){
+Car ferrari  = new Car("ferrari","car",2009,"black",4,5);
+    System.out.println(ferrari.wheels);
+    System.out.println(ferrari.name);
+    System.out.println(ferrari.getWheels());
+}
+}
---
+++ Inheritence/Vehicle.java
@@ -0,0 +1,25 @@
+package Inheritence;
+
+/**
+ * Created by enilmaa on 2017-06-22.
+ */
+public class Vehicle {
+
+    protected String name;
+    private int year;
+    private String color;
+    private String type;
+    protected int wheels;
+    public Vehicle(String name, int year, String color, String type,int wheels){
+        this.color=color;
+        this.name=name;
+        this.type=type;
+        this.year=year;
+        this.wheels=wheels;
+    }
+
+    public String getWheels(){
+        return "this is from vehicle class";
+    }
+
+}
